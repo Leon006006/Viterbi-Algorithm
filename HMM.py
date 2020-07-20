11281f0ae47e1bf939730aa6fa96db1296428c12
Leon006006 <50409264+Leon006006@users.noreply.github.com>
2020-07-20 12:43:18 +0200

    commented version

--- HMM.py
+++ HMM.py
@@ -3,38 +3,56 @@ import numpy as np
 
 class HMM:
     def __init__(self, A, B, Pi):
+        """
+        :param A: Transition probabilities
+        :param B: Emission probabilities
+        :param Pi: Starting probabilities
+        """
         self.A = A
         self.B = B
         self.Pi = Pi
 
     def viterbi(self, Observable):
+        """
+        :param Observable: Observed sequence
+        :return: Most probable status sequence
+        """
+
+        # Table for probabilities and predecessor
         array = np.zeros((self.A.shape[0], Observable.shape[1], 2))
+        # Number of statuses
         numb_status = array.shape[0]
+        # Number of points in time
         T = array.shape[1]
 
+        # Compute probabilities for first point in time
         for x in range(0, numb_status):
             array[x, 0, 0] = self.Pi[x] * self.B[x, Observable[0, 0]]
-            array[x, 0, 1] = 20
+            array[x, 0, 1] = -1     # Predecessor on "random" value
 
+        # Compute all necessary probabilities
         for t in range(1, T):
             for status in range(0, numb_status):
                 v = np.zeros((numb_status, 1))
                 for pre_status in range(0, numb_status):
                     v[pre_status, 0] = array[pre_status, t-1, 0] * self.A[pre_status, status] * \
                                        self.B[status, Observable[0, t]]
+                # Find maximum probability for given observation
                 array[status, t, 0] = max(v)
                 array[status, t, 1] = np.argmax(v)
 
+        # Get highest probability of observation
         highest_prop = np.argmax(array[:, T-1, 0])
         probability_observation = array[highest_prop, T-1, 0]
-
         print("Highest probability for observation-sequence: {}%".format(np.round(probability_observation * 100, 2)))
 
+        # Initialize sequence of states vector
         status_seq = np.zeros((1, T))
         status_seq[0, T-1] = highest_prop
 
+        # Compute and save status sequence
         for t in range(1, T):
             g = status_seq[0, T-t].astype(int)
             status_seq[0, T-t-1] = array[g, T-t, 1]
 
-        print("Status sequenz for highest probability: {}".format(status_seq))
+        return status_seq
--- Test.py
+++ Test.py
@@ -1,12 +1,16 @@
 import numpy as np
 import HMM as HMM
 
-
+# Initialize required matrices and the HMM
 A = np.array([[0.8, 0.2], [0.1, 0.9]])
 B = np.array([[0.7, 0.3], [0.2, 0.8]])
 Pi = np.array([[0.5], [0.5]])
-Observation = np.array([[0, 1, 0]])
-
 HMM1 = HMM.HMM(A, B, Pi)
 
-HMM1.viterbi(Observation)
+# Initialize observed vector
+Observation = np.array([[0, 1, 0]])
+
+# Run Viterbi algorithm
+status_seq = HMM1.viterbi(Observation)
+
+print("Status sequence with highest probability: {}".format(status_seq))
